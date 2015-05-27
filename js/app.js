c1cfd2e8e0d710ef537fe905d2b183c8d2dd8912
bryantee <bryantee@gmail.com>
2015-05-27 15:40:50 -0700

    Starting on jQuery, in progress, saving for work at home.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+	console.log('document ready');
+
+	$('.addButton').on('click', function() {
+		var item = $('.addItems').val();
+		console.log('Adding ' + item);
+		//$('.toBuy').append()
+	});
+
+});
