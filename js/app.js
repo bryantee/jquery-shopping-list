9947703a0389dae7ab3c0298264a9a025ac7965a
Bryan Swagerty <bryantee@gmail.com>
2015-05-28 07:16:17 -0700

    hover and select now working
    Had to use delegated events to select child elements of .itemlist so
    elements added after DOM loads could be selected for.

--- js/app.js
+++ js/app.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
 	console.log('document ready');
 
+	// add items to list
+
 	$('.addButton').on('click', function() {
 		var item = $('.addItems').val();
 		console.log('Adding ' + item);
-		//$('.toBuy').append()
+		$('.toBuy ul').append('<li class="itembuy">' + item + '</li>');
+	});
+
+	// hover: highlight to add to cart
+
+	$('.itemlist').on('mouseenter', 'li', function() {
+		console.log('mouse entered item ' + $(this).html())
+		$(this).addClass('selected');
+		});
+	$('.itemlist').on('mouseleave', 'li', function() {
+		$(this).removeClass('selected');
+	});
+
+	// on click: remove from list -> add to cart
+
+	$('.itemlist').on('click', 'li', function() {
+		var itemToCart = $(this).text();
+		$(this).remove();
+		console.log('removed ' + $(this).html());
+		$('.cart ul').append('<li class="incart">' + itemToCart + '</li>');
 	});
 
 });
